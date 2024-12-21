a9d2b914b4177bf01993f5114f769ca49666d1a0
sohamnilvaze <147164236+sohamnilvaze@users.noreply.github.com>
2024-12-21 16:57:51 +0530

    Added Comments

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from functions import load_detector, load_image,make_prediction, majority_label
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -22,40 +23,33 @@ def predict():
 
     detector = load_detector()
     image=load_image(image_url)
+
+    
     
     # predictions= make_prediction(detector,image, candidate_labels)
-    predictions=detector(image,candidate_labels=candidate_labels)
+    predictions,labels,scores=detector(image,candidate_labels=candidate_labels)
+
+    plt.figure(figsize=(8, 6))
+    plt.bar(labels, scores, color='skyblue', edgecolor='black')
+
+    # Add labels and title
+    plt.xlabel('Labels', fontsize=14)
+    plt.ylabel('Scores', fontsize=14)
+    plt.title('Scores for Each Label', fontsize=16)
+    plt.ylim(0, 1)  # Assuming scores are between 0 and 1
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+    plt.tight_layout()
+    plt.savefig('/plots/bar_chart.png',dpi=300, bbox_inches='tight')
+
     majority=majority_label(predictions)
     return render_template('result.html',
                            image_url = image_url,  
                            predicted_label=predictions,  
-                           match=majority)
+                           match=majority,
+                           labels=labels,
+                           scores=scores)
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     if 'image_file' not in request.files:
-#         return "No image uploaded", 400
-
-#     image_file = request.files['image_file'].read()
-#     image = Image.open(file.stream).convert("RGB")
-#     label1=request.form['label1']
-#     label2=request.form['label2']
-#     label3=request.form['label3']
-#     label4=request.form['label4']
-#     candidate_labels =[]
-#     candidate_labels.append(label1)
-#     candidate_labels.append(label2)
-#     candidate_labels.append(label3)
-#     candidate_labels.append(label4)
-
-#     detector = load_detector()
-#     # image=load_image(image_url)
-    
-#     # predictions= make_prediction(detector,image_file, candidate_labels)
-#     predictions=detector(image_file,candidate_labels=candidate_labels)
-#     majority = majority_label(predictions)
-#     return render_template('result.html',   
-#                            match=majority)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- functions.py
+++ functions.py
@@ -4,23 +4,31 @@ import requests
 
 checkpoint = "openai/clip-vit-large-patch14"
 
+#Function to load the detector from the checkpoint
 def load_detector():
     detector = pipeline(model=checkpoint, task="zero-shot-image-classification")
     return detector
 
+#Function to load the image from the url
 def load_image(image_url):
     image = Image.open(requests.get(image_url, stream=True).raw)
     return image
 
+#Function to predict the label to which image belongs to 
 def make_prediction(detector,image,candidate_labels):
     prediction =  detector(image,candidate_labels)
     return prediction
 
+#Function ti determine the majority label from the predictons
 def majority_label(prediction):
     prob = 0.0
+    labels=[]
+    scores=[]
     for i in range(len(prediction)):
+        labels.append(prediction[i]['label'])
+        scores.append(prediction[i]['score'])
         if(prob < prediction[i]['score']):
             selected = prediction[i]['label']
             prob = prediction[i]['score']
     
-    return selected
+    return selected,labels,scores
